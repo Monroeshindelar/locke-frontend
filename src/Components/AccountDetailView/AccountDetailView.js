ec91556522191bb4b47ae18c62d6fd7dd7cbd1a4
Monroe Shindelar <monroeshindelar@gmail.com>
2021-08-19 19:36:41 -0700

    LW-12-Get-Users-Games

--- src/Components/AccountDetailView/AccountDetailView.js
+++ src/Components/AccountDetailView/AccountDetailView.js
@@ -3,6 +3,8 @@ import "./AccountDetailView.css";
 import { Link } from "react-router-dom";
 import { DISCORD_AVATAR_BASE_URL } from "../../constants";
 import { getGamesByUserId } from "../../Utilities/GameServiceApiUtils";
+import { Table } from "reactstrap";
+import { GAME_DETAIL_VIEW_PATH } from "../../constants";
 
 class AccountDetailView extends Component {
   constructor(props) {
@@ -23,13 +25,58 @@ class AccountDetailView extends Component {
     return `${DISCORD_AVATAR_BASE_URL}/${this.props.account.principalId.toString()}/${this.props.account.avatar.toString()}.png`;
   }
 
+  handleGameIdClick(gameId) {
+
+  }
+
   render() {
+    const games = [];
+
+    var headerStyle = {
+      color: "white"
+    };
+
+    if (this.state.games) {
+      for(var g in this.state.games) {
+        var game = this.state.games[g];
+        
+        games.push(
+          <tr>
+            <th style={headerStyle}><Link to={{
+              pathname: GAME_DETAIL_VIEW_PATH,
+              state: { gameId: game.id }
+            }}>{game.id}</Link></th>
+            <td style={headerStyle}>{game.settings.name}</td>
+            <td style={headerStyle}>{game.gameState.gameStateType}</td>
+            <td style={headerStyle}>{game.settings.generationId}</td>
+            <td style={headerStyle}>{game.settings.difficultyMode}</td>
+            <td style={headerStyle}>{game.settings.accessibility}</td>
+          </tr>
+        )
+      }
+    }
+
     return (
       <div className="AccountDetailView">
         <header className="App-header">
           <p>Account Detail</p>
           <p>Username: {this.props.account.username}#{this.props.account.discriminator}</p>
           <img src={this.getAvatarUrl()} alt=""></img>
+          <Table>
+            <thead>
+              <tr>
+                <th style={headerStyle}>Id</th>
+                <th style={headerStyle}>Name</th>
+                <th style={headerStyle}>State</th>
+                <th style={headerStyle}>Generation</th>
+                <th style={headerStyle}>Difficulty</th>
+                <th style={headerStyle}>Accessibility</th>
+              </tr>
+            </thead>
+            <tbody>
+              {games}
+            </tbody>
+          </Table>
           <Link to="/">Go Back to Home Page</Link>
         </header>
       </div>
