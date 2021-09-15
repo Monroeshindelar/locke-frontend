f35e34559e6919a64e61ee527d06eb3a6a13b7ce
TannerDryden <tdd7197@gmail.com>
2021-09-14 21:44:06 -0700

    added docker-compose

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,11 @@ export const ACCESS_TOKEN_NAME = "access_token";
 //export const API_BASE_URL = "http://10.0.0.84:9292";
 export const API_BASE_URL = "http://24.17.77.207:9292";
 
-export const OAUTH2_REDIRECT_URL = "http://localhost:3000/oauth2/redirect";
+export const OAUTH2_REDIRECT_URL = "http://24.17.77.207:3000/oauth2/redirect";
 export const DISCORD_AVATAR_BASE_URL = "https://cdn.discordapp.com/avatars/";
 export const DISCORD_AUTH_URL = `${API_BASE_URL}/oauth2/authorization/discord?redirect_uri=${OAUTH2_REDIRECT_URL}`;
 
-export const ACCOUNT_DETAIL_VIEW_PATH = "/accounts/me"
-export const GAME_DETAIL_VIEW_PATH = "/game"
-export const GAME_CREATION_CONFIGURATION_PATH = "/game/create"
-export const GAME_PARTICIPANT_DETAIL_VIEW_PATH = "/game/participant"
+export const ACCOUNT_DETAIL_VIEW_PATH = "/accounts/me";
+export const GAME_DETAIL_VIEW_PATH = "/game";
+export const GAME_CREATION_CONFIGURATION_PATH = "/game/create";
+export const GAME_PARTICIPANT_DETAIL_VIEW_PATH = "/game/participant";
