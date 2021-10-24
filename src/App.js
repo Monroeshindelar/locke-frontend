f12ad86a41ee0ad365fb45107976c776a42105d2
Monroe Shindelar <monroeshindelar@gmail.com>
2021-10-23 17:00:11 -0700

    LW-21 Logout. The CSS is horrible, need to fix later.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import { withProps } from "./Utilities/Utils"
 import { Component } from "react";
 import GameCreationView from "./Components/GameCreationView/GameCreationView";
 import { ACCOUNT_DETAIL_VIEW_PATH, GAME_DETAIL_VIEW_PATH, GAME_CREATION_CONFIGURATION_PATH, GAME_PARTICIPANT_DETAIL_VIEW_PATH } from "./constants";
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { getUsernameWithDiscriminator } from "./Utilities/AuthServiceApiUtils";
+import { DISCORD_AUTH_URL } from "./constants";
+import { getAvatarUrl } from "./Utilities/UserUtils" 
+import { ACCESS_TOKEN_NAME } from "./constants";
+import "./App.css"
+
 
 class App extends Component {
   constructor(props) {
@@ -43,22 +50,64 @@ class App extends Component {
     }
   }
 
+  logout() {
+    localStorage.removeItem(ACCESS_TOKEN_NAME);
+    this.setState({
+      user: null
+    });
+  }
+
   render() {
     if(this.state.loading) {
       return <h1>Loading</h1>
     }
 
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={withProps(Home, { user: this.state.user })}/>
-          <Route path={GAME_DETAIL_VIEW_PATH} exact component={withProps(GameDetailView, { user: this.state.user })} />
-          <PrivateRoute path={ACCOUNT_DETAIL_VIEW_PATH} authenticated={isAuthenticated() && this.state.user} account={this.state.user} exact component={AccountDetailView} />
-          <Route path={GAME_PARTICIPANT_DETAIL_VIEW_PATH} exact component={GameParticipantDetailView} />
-          <Route path={GAME_CREATION_CONFIGURATION_PATH} exact component={GameCreationView} />
-          <Route path="/oauth2/redirect" exact component={OAuth2RedirectHandler} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand class="brandHeader">Locke Webapp</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav class="me-auto">
+                {
+                  this.state.user
+                  ?
+                    <Nav.Link href={GAME_CREATION_CONFIGURATION_PATH}>Create Game</Nav.Link>
+                  :
+                    ""
+                }
+              </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+                {
+                  this.state.user 
+                  ? 
+                    <div className="userCard">
+                      {/* <Navbar.Text>{getUsernameWithDiscriminator(this.state.user)}</Navbar.Text> */}
+                      <NavDropdown title={getUsernameWithDiscriminator(this.state.user)}>
+                        <NavDropdown.Item href="/accounts/me">Profile</NavDropdown.Item>
+                        <NavDropdown.Item href="/" onClick={() => this.logout()}>Logout</NavDropdown.Item>
+                      </NavDropdown>
+                      <img className="userAvatar" src={getAvatarUrl(this.state.user)} alt="#" width="50" height="50"/>
+                    </div>
+                  : 
+                    <a href={DISCORD_AUTH_URL} className="btn btn-primary">Log in with Discord</a>
+                }
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={withProps(Home, { user: this.state.user })}/>
+            <Route path={GAME_DETAIL_VIEW_PATH} exact component={withProps(GameDetailView, { user: this.state.user })} />
+            <PrivateRoute path={ACCOUNT_DETAIL_VIEW_PATH} authenticated={isAuthenticated() && this.state.user} account={this.state.user} exact component={AccountDetailView} />
+            <Route path={GAME_PARTICIPANT_DETAIL_VIEW_PATH} exact component={GameParticipantDetailView} />
+            <Route path={GAME_CREATION_CONFIGURATION_PATH} exact component={GameCreationView} />
+            <Route path="/oauth2/redirect" exact component={OAuth2RedirectHandler} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
---
+++ src/Utilities/UserUtils.js
@@ -0,0 +1,6 @@
+import { DISCORD_AVATAR_BASE_URL } from "../constants";
+
+
+export const getAvatarUrl = (user) => {
+    return `${DISCORD_AVATAR_BASE_URL}/${user.principalId.toString()}/${user.avatar.toString()}.png`;
+}
